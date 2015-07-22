bc684f4f7824a737b51df73c5f3f9fa7176f9246
Jeet Desai <desaijeetd@gmail.com>
2015-07-22 11:59:17 +0530

    WordPress custom theme uploaded

---
+++ zerif-lite-child/sections/testimonials.php
@@ -0,0 +1,19 @@
+<?php
+
+
+			echo '<section class="testimonial" id="testimonials">';
+
+
+				echo '<div class="container">';
+
+					//Image Slider Code
+					wd_slider(1);
+
+
+				echo '</div>';
+
+
+			echo '</section>';
+
+
+?>
